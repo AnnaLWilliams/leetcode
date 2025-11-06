431a08fc0560235a18d29750d16d54093a4f9236
AnnaLWilliams <williamsab2718@gmail.com>
2025-11-06 00:37:54 -0500

    Added basic tree functionality.  Need to add wild card handling next

--- 211.design-add-and-search-words-data-structure.cs
+++ 211.design-add-and-search-words-data-structure.cs
@@ -54,7 +54,7 @@
  * Constraints:
  * 
  * 
- * 1 <= word.length <= 25
+ * 1 <= word.Length <= 25
  * word in addWord consists of lowercase English letters.
  * word in search consist of '.' or lowercase English letters.
  * There will be at most 2 dots in word for search queries.
@@ -65,17 +65,121 @@
 
 // @lc code=start
 public class WordDictionary {
-
+    Node tree;
     public WordDictionary() {
-        
+        tree = new Node("");
     }
     
     public void AddWord(string word) {
-        
+        if (tree.value == "")
+        {
+            tree.value = word;
+        }
+        else
+        {
+            var wordOrder = 0;
+            var node = tree;
+            //Find an empty branch
+            while(true)
+            {
+                wordOrder = CalcWordOrder(node.value, word);
+
+                if (wordOrder == -1)//a copy of the word has been found
+                {
+                    break;
+                }
+                else if (node.children[wordOrder] == null) //An empty branch for the word has been found
+                {
+                    node.children[wordOrder] = new Node(word);
+                    break;
+                }
+                else //There is already a child node so move to that node
+                {
+                    node = node.children[wordOrder];
+                }
+            }
+        }
+    }
+
+    public bool Search(string word)
+    {
+        var wordOrder = 0;
+        var node = tree;
+        while(true)
+        {
+            wordOrder = CalcWordOrder(node.value, word);
+
+            if (wordOrder == -1)//a copy of the word has been found
+            {
+                return true;
+            }
+            else if (node.children[wordOrder] == null) //An empty branch has been found
+            {
+                return false;
+            }
+            else //There is already a child node so move to that node
+            {
+                node = node.children[wordOrder];
+            }
+        }
     }
     
-    public bool Search(string word) {
-        
+    /// <summary>
+    /// Checks the alphabetical order of two words
+    /// </summary>
+    /// <param name="rootWord"></param>
+    /// <param name="branchWord"></param>
+    /// <returns>
+    /// -2 the word contains a dot so both branches need to be checked
+    /// -1 = the words are the same
+    /// 0 Branch word is alphabetically before Root word
+    /// 1 Root word is alphabetically before Branch word
+    /// </returns>
+    public int CalcWordOrder(string rootWord, string branchWord)
+    {
+        for (var index = 0; index < rootWord.Length && index < branchWord.Length; index++)
+        {
+            if (branchWord[index] == '.')
+            {
+                return -2;
+            }
+            else if (rootWord[index] < branchWord[index])
+            {
+                return 1;
+            }
+            else if (rootWord[index] > branchWord[index])
+            {
+                return 0;
+            }
+        }
+
+        //The start of the words are the same calculate based on Length
+        if (rootWord.Length > branchWord.Length)
+        {
+            return 0;
+        }
+        else if (rootWord.Length < branchWord.Length)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+
+
+    }
+}
+
+public class Node
+{
+    public string value;
+    public Node?[] children;
+
+    public Node(string value)
+    {
+        this.value = value;
+        children = new Node?[2];
     }
 }
 
